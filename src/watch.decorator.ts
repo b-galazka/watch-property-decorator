a086a28e2929e54d501f9ccbb58cf846806c3543
b-galazka <bartlomiejgalazka@zoho.eu>
2019-05-25 11:57:26 +0200

    fix watching single field by multiple methods

--- src/watch.decorator.ts
+++ src/watch.decorator.ts
@@ -6,22 +6,30 @@ export function Watch<T = any>(propName: keyof T, options: WatchOptions = { imme
     return (target: object, methodName: string): any => {
         const propertyDescriptor = Object.getOwnPropertyDescriptor(target, propName);
 
-        let setter: (value: any) => boolean;
-
         if (propertyDescriptor) {
-            setter = function set(value: any): boolean {
-                const prevValue = propertyDescriptor.get.call(this);
-                const isInitialValue = propertyDescriptor.set.call(this, value);
-                const method = <() => any> this[methodName];
+            return Object.defineProperty(target, propName, {
+                set(value: any): boolean {
+                    const prevValue = propertyDescriptor.get.call(this);
+                    const isInitialValue = propertyDescriptor.set.call(this, value);
+                    const method = <() => any> this[methodName];
 
-                if (method && (!isInitialValue || options.immediate)) {
-                    method.call(this, value, prevValue, isInitialValue);
-                }
+                    if (method && (!isInitialValue || options.immediate)) {
+                        method.call(this, value, prevValue, isInitialValue);
+                    }
 
-                return isInitialValue;
-            };
-        } else {
-            setter = function set(value: any): boolean {
+                    return isInitialValue;
+                },
+
+                get(): any {
+                    return propertyDescriptor.get.call(this);
+                },
+
+                configurable: true,
+            });
+        }
+
+        Object.defineProperty(target, propName, {
+            set(value: any): boolean {
                 const prevValue = this[valueKey];
                 const isInitialValue = !this.hasOwnProperty(valueKey);
                 const method = <() => any> this[methodName];
@@ -33,16 +41,13 @@ export function Watch<T = any>(propName: keyof T, options: WatchOptions = { imme
                 }
 
                 return isInitialValue;
-            };
-        }
-
-        Object.defineProperty(target, propName, {
-            set: setter,
-            configurable: true,
+            },
 
             get(): any {
                 return this[valueKey];
-            }
+            },
+
+            configurable: true
         });
     };
 }
